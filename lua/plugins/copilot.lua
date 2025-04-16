45dbc8a6f70fc174525b6b2ce65fce6bd93c753f
Janne Nowak <janne.now@icloud.com>
2025-04-16 23:18:32 +0200

    working copilot only suggestion and blink

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,57 @@
+return {
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup {}
+  --   end,
+  -- },
+  {
+     "github/copilot.vim",
+     lazy = false,
+     config = function()
+       -- Mapping tab is already used by NvChad
+       -- vim.g.copilot_no_tab_map = true
+       -- vim.g.copilot_assume_mapped = true
+       -- vim.g.copilot_tab_fallback = ""
+       -- The mapping is set to other key, see custom/lua/mappings
+       -- or run <leader>ch to see copilot mapping section
+     end,
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "fang2hou/blink-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+      keymap = {
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+      },
+    },
+  },
+}
