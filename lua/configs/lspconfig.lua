36f907b6ed3f2001ef6556a2ee2d83cd4694779e
J.Nowak <j.nowak@maerz-network.de>
2025-04-16 08:49:11 +0200

    Fixed copilot and normal tabs

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local lspconfig = require "lspconfig"
 
 nvlsp.defaults() -- loads nvchad's defaults
 
-local servers = { "html", "cssls", "pyright", "jsonls", "bashls", "gitlab_ci_ls" }
+local servers = { "html", "cssls", "pyright", "jsonls", "bashls", "gitlab-ci-ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
