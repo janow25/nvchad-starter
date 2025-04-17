32746617b6b4854636709c2d3980b2a0a76eb57d
Janne Nowak <janne.now@icloud.com>
2025-04-17 15:35:45 +0200

    Allowing Copilot to write commit messages

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -11,12 +11,21 @@ return {
     "github/copilot.vim",
     lazy = false,
     config = function()
-      -- Mapping tab is already used by NvChad
+      -- Uncomment these if you want to use a different mapping for Tab
       -- vim.g.copilot_no_tab_map = true
       -- vim.g.copilot_assume_mapped = true
       -- vim.g.copilot_tab_fallback = ""
+
+      -- Enable Copilot for commit files
+      vim.g.copilot_filetypes = {
+        ["*"] = true, -- Enable for all filetypes by default
+        ["gitcommit"] = true, -- Explicitly enable for git commit messages
+        ["markdown"] = true, -- Example: Enable for Markdown files
+        -- Add more filetypes here if needed
+      }
+
       -- The mapping is set to other key, see custom/lua/mappings
-      -- or run <leader>ch to see copilot mapping section
+      -- or run <leader>ch to see Copilot mapping section
     end,
   },
   {
