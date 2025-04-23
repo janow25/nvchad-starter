bc9b882645241fe11afef4ac6daff78a0a6d8613
J.Nowak <j.nowak@maerz-network.de>
2025-04-23 22:45:48 +0200

    Refactor options and add vim-tmux-navigator plugin configuration

--- lua/options.lua
+++ lua/options.lua
@@ -2,8 +2,21 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local opt = vim.opt
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
 
 -- show lsp infos in insert mode
 vim.diagnostic.config({
