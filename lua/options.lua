90d5cc7070c102676877b1983d9b7b73d0fd6992
J.Nowak <j.nowak@maerz-network.de>
2025-04-28 14:10:44 +0200

    Try to add iris syntax

--- lua/options.lua
+++ lua/options.lua
@@ -22,3 +22,12 @@ opt.swapfile = false
 vim.diagnostic.config({
   update_in_insert = true,
 })
+
+vim.api.nvim_create_autocmd({"FileType"}, {
+  pattern = "objectscript",
+  callback = function()
+    -- Clear existing syntax and source your custom syntax file
+    vim.cmd("syntax clear")
+    vim.cmd("source ~/.config/nvim/syntax/objectscript.vim")
+  end,
+})
